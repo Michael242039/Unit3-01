388d57b6a3f80ac874a1bee3e644ad4379056eb6
Michael Taylor <michaeltaylorr02@gmail.com>
2017-10-03 14:26:07 -0400

    initial

---
+++ guessing_game.py
@@ -0,0 +1,21 @@
+#Created by: Michael Taylor
+#Created on: October 2, 2017
+#Created for ICS3U
+#This program is a number guessing game 
+
+import ui
+
+CORRECT_ANSWER = 30
+
+def check_answer_button(sender):
+	#checks if user input equals CORRECT_ANSWER
+	
+	user_guess = int(view['user_guess_input'].text)
+	
+	if user_guess == CORRECT_ANSWER:
+		view['output_text'].text = 'Correct!'
+	else:
+		view['output_text'].text = 'Incorrect!'
+
+view = ui.load_view()
+view.present('sheet')
